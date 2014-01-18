a5d6bd1c0da51564b9035e492f85f9bf230120b0
claudiu <claudiu.garba@gmail.com>
2014-01-19 00:37:08 +0100

    clean

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -8,12 +8,12 @@ describe Trip do
   it {should have_field(:long_description)}
   it {should have_field(:seo_description)}
   it {should have_field(:internal_status)}
-  it {shoud have_field(:rating)}
+  it {should have_field(:rating)}
   it {should have_field(:owner)}
   it {should have_field(:created_at)}
   it {should have_field(:updated_at)}
 
-  it {should validate_presense_of(:name)}
+  it {should validate_presence_of(:name)}
   it {should validate_presense_of(:title)}
   it {should validate_presense_of(:active)}
   it {should validate_presense_of(:short_description)}
@@ -38,8 +38,8 @@ describe Trip do
   it {should have_field(:created_at).of_type(DateTime)}  
   it {should have_field(:updated_at).of_type(DateTime)}  
 
-  it {should have_index_for(name: 1)}
-  it {should have_index_for(title: 1)}
+  it {should have_index_for(name: 1).with_options(unique: true)}
+  it {should have_index_for(titlw: 1).with_options(unique: true)}
 
   it {should validate_length_of(:name).within(4..20)}
   it {should validate_length_of(:title).within(4..20)}
