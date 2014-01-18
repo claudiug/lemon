be0162a90aaec00ffd9af370349a2b8ce85a8709
claudiu <claudiu.garba@gmail.com>
2014-01-19 00:26:00 +0100

    create trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,40 @@
 class Trip
   include Mongoid::Document
+
+  field :name, type: String
+  field :title, type: String
+  field :active, type: Boolean, default: false
+  field :short_description, type: String
+  field :long_description, type: String
+  field :seo_description, type: Array
+  field :internal_status, type: String
+  field :rating, type: Integer
+  field :owner, type: String
+  field :created_at, type: DateTime
+  field :updated_at, type: DateTime
+
+  validates :name, presence: true
+  validates :title, presence: true
+  validates :active, presence: true
+  validates :short_description, presence: true
+  validates :long_description, presence: true
+  validates :seo_description, presence: true
+  validates :internal_status, presence: true
+  validates :rating, presence: true
+  validates :owner, presence: true
+
+  validates :name, uniqueness: true
+  validates :title, uniqueness: true
+
+  validates :name, length: {within:(4..20)} 
+  validates :title, length: {within:(4..20)} 
+  validates :long_description, length: {within:(10..500)} 
+  validates :short_description, length: {within:(10..100)} 
+  validates :seo_description, length: {within:(10..100)} 
+  validates :rating, length: {within:(1..10)} 
+  validates :rating, numericality:{only_integer: true}
+
+  index({ name: 1 }, { unique: true})
+  index({ title: 1 }, { unique: true})
+
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -9,10 +9,10 @@ describe Trip do
   it {should have_field(:seo_description)}
   it {should have_field(:internal_status)}
   it {shoud have_field(:rating)}
-  it {should have_field(:datetime)}
   it {should have_field(:owner)}
   it {should have_field(:created_at)}
   it {should have_field(:updated_at)}
+
   it {should validate_presense_of(:name)}
   it {should validate_presense_of(:title)}
   it {should validate_presense_of(:active)}
@@ -22,10 +22,11 @@ describe Trip do
   it {should validate_presense_of(:internal_status)}
   it {should validate_presense_of(:rating)}
   it {should validate_presense_of(:owner)}
+
   it {should validate_uniqueness_of(:name)}
   it {should validate_uniqueness_of(:title)}
+
   it {should have_field(:active).of_type(Boolean).with_default_value_of(false)}
-  it {should have_field(:active).of_type(Datetime)}
   it {should have_field(:name).of_type(String)}  
   it {should have_field(:title).of_type(String)}  
   it {should have_field(:short_description).of_type(String)}  
@@ -33,15 +34,18 @@ describe Trip do
   it {should have_field(:seo_description).of_type(Array)}  
   it {should have_field(:internal_status).of_type(String)}  
   it {should have_field(:rating).of_type(Integer)}  
-  it {should have_field(:rating).of_type(String)}  
   it {should have_field(:owner).of_type(String)}  
+  it {should have_field(:created_at).of_type(DateTime)}  
+  it {should have_field(:updated_at).of_type(DateTime)}  
+
   it {should have_index_for(name: 1)}
   it {should have_index_for(title: 1)}
+
   it {should validate_length_of(:name).within(4..20)}
   it {should validate_length_of(:title).within(4..20)}
   it {should validate_length_of(:long_description).within(10..500)}
   it {should validate_length_of(:short_description).within(10..100)}
   it {should validate_length_of(:seo_description).within(10..100)}
-  it {should validate_numericality_of(:rating).only_integer: true}
+  it {should validate_numericality_of(:rating).to_allow.(only_integer: true)}
   it {should validate_numericality_of(:rating).within(1..10)}
 end
