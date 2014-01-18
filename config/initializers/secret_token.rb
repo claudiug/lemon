6753ee1d210a47e22b30f4e5078658f51fc118a4
claudiu <claudiu.garba@gmail.com>
2014-01-18 22:21:25 +0100

    add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lemon::Application.config.secret_key_base = '89e2999f438e946c8d7b9ddb831e4d1af2ffa2468bdd39e8157975666d647440ff9eaff5af9a9c7f2a1eada305ef04f23985cd4df4b47f5e90882edb995968b4'
